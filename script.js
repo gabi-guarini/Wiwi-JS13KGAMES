f71a1be072b8e93e4e3718e78400dc6096c1f6bb
Gabriela Guarini <124116656+gabi-guarini@users.noreply.github.com>
2025-09-01 02:01:59 -0300

    fix asset sprite and added logic animation

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 400;
 
-const catVoidImage = new Image();
-catVoidImage.src = './assets/cat_void.png';
+const catImage = new Image();
+catImage.src = './assets/cat.png';
+
 const spriteWidth = 32;
 const spriteHeight = 32;
+let playerState = 'idle';
 
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
-let staggerFrames = 15;
+const staggerFrames = 14;
+const spriteAnimations = [];
+const animationStates = [
+  { name: 'stand', frames: 4 },
+  { name: 'sit', frames: 4 },
+  { name: 'idle', frames: 8 },
+  { name: 'walk', frames: 8 },
+  { name: 'jump', frames: 8 },
+  { name: 'void', frames: 14 },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let x = j * spriteWidth;
+    let y = index * spriteHeight;
+    frames.loc.push({ x, y });
+  }
+  spriteAnimations[state.name] = frames;
+});
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(catVoidImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth * 1.5, spriteHeight * 1.5);
-  if (gameFrame % staggerFrames === 0) {
-    frameX < 14 ? frameX++ : frameX = 0;
-  }
+  let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
+
+  ctx.drawImage(catImage, frameX, frameY,
+    spriteWidth, spriteHeight, 0, 0, spriteWidth * 1.2, spriteHeight * 1.2);
+
   gameFrame++;
   requestAnimationFrame(animate);
 }
 
-catVoidImage.onload = () => {
+catImage.onload = () => {
   animate();
 };
